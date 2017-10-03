5604583eb54c7f73b7e6ad7495d8188e675b1d23
Alex <a.peran@orange.fr>
2017-10-03 16:40:07 +0200

    exo 5.2

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,7 +24,8 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+        List<Customer> result = orders.stream().map(order -> order.getCustomer())
+        		.distinct().collect(Collectors.toList());
 
         assertThat(result, hasSize(2));
     }
@@ -36,7 +37,9 @@ public class Stream_02_Test {
 
         // TODO calculer les statistiques sur les prix des pizzas vendues
         // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
+        IntSummaryStatistics result = orders.stream()
+        		.flatMap(pizza -> pizza.getPizzas().stream())
+        		.collect(Collectors.summarizingInt(Pizza::getPrice));
 
 
         assertThat(result.getSum(), is(10900L));
